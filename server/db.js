5fc9857a39b704672db36192ffc81a27e88c2ddf
ajith736 <ajithc736@gmail.com>
2024-08-31 20:05:33 +0530

    deployment config added

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    user:"postgres",
-    password:"Ajith@2003",
-    host:"localhost",
-    port:5432,
-    database:"perntodo"
+// const pool = new Pool({
+//     user:"postgres",
+//     password:"Ajith@2003",
+//     host:"localhost",
+//     port:5432,
+//     database:"perntodo"
 
-});
+// });
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+  })
 
 module.exports = pool;
